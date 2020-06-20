7b942a17e3761acb5cfcef767c5ac02a28a85bd8
Santiago Pulido Pelaez <santiagopulidopelaez@gmail.com>
2020-06-20 16:03:01 -0500

    Nueva fuente de citas, la api falló

--- src/App.js
+++ src/App.js
@@ -1,59 +1,59 @@
-import React, { Component } from 'react'
-import './App.css'
-import Cita from './components/Cita'
-import Autor from './components/Autor'
-import Change from './components/Boton'
+import React, { Component } from "react";
+import "./App.css";
+import Cita from "./components/Cita";
+import Autor from "./components/Autor";
+import Change from "./components/Boton";
+import quoteMock from "./mocks/quotes.json";
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-           citas: [],
-           quote : '' ,
-           autor: ''
-         }
-                    
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      citas: [],
+      quote: "",
+      autor: "",
+      loading: true,
+    };
+  }
 
-    showCitas(){
-        console.log(this.state.citas)
-    }
+  showCitas() {
+    console.log(this.state.citas);
+  }
 
-    componentDidMount(){
-        fetch('https://type.fit/api/quotes')
-        .then(response=> response.json())
-        .then(data=> {
-            
-            this.setState({
-                citas:data,
-                quote:data[0].text,
-                autor:data[0].author
-                
-            })
-           
-        });
-        
-    }
+  componentDidMount() {
+    this.setState({
+      citas: quoteMock,
+      quote: quoteMock[0].text,
+      autor: quoteMock[0].author,
+      loading: false,
+    });
+  }
 
-    hola = () => {
-        let a = Math.floor(Math.random()*(1643));
-        this.setState({
-            quote:this.state.citas[a].text,
-            autor:this.state.citas[a].author
-        });
-        
-       
+  hola = () => {
+    let a = Math.floor(Math.random() * 522);
+    this.setState({
+      quote: this.state.citas[a].text,
+      autor: this.state.citas[a].author,
+    });
+  };
+  render() {
+    if (!this.state.loading) {
+      return (
+        <div className="App">
+          <Cita quote={this.state.quote} />
+          <Autor author={this.state.autor} />
+          <Change
+            name="Next Quote"
+            funHola={this.hola}
+            cita={this.state.quote}
+            autor={this.state.autor}
+          />
+        </div>
+      );
+    } else {
+      return <div>Cargando...</div>;
     }
-        
-    render() {     
-        return ( 
-            <div className='App'>              
-               <Cita quote = {this.state.quote}/>
-               <Autor author = {this.state.autor}/>
-               <Change name = 'Next Quote' funHola = {this.hola} cita={this.state.quote} autor={this.state.autor}/>
-            </div>
-        );
-    }
+  }
 }
- 
+
 export default App;
